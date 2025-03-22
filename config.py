76fc5b0e8746bcd4ff57cec7ce4da46027fadb09
Tom Morara <morara1920@gmail.com>
2025-03-22 05:22:55 +0300

    Adjusted app configuration settings

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://laundry_user:1290@localhost/laundry_db'
+
+    # ✅ Correct database name
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1290@localhost/laundry_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
